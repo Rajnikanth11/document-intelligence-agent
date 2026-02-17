a2d014e9e602e896ac28d0d6430ea27b4257482f
Rajnikanth Kausalya <50887542+Rajnikanth11@users.noreply.github.com>
2026-02-17 20:22:48 +0530

    updated with path
    Updated the Tesseract command path to use a consistent format.

--- ingestion/pdf_loader.py
+++ ingestion/pdf_loader.py
@@ -1,8 +1,8 @@
 from pdf2image import convert_from_path
 import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-pytesseract.pytesseract.pytesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def pdf_to_text(pdf_path: str) -> str:
     pages = convert_from_path(pdf_path, poppler_path=r"C:\Users\kausa\OneDrive\Documents\personal\Release-25.12.0-0\poppler-25.12.0\Library\bin")
